ad08a1218d1829ad18a9f06c652b8be71e0525d7
hyemdev <devdev.hyem@gmail.com>
2023-09-18 12:40:17 +0900

    feat : json 파일 로드 완료

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,30 @@
-import Header from "@/components/common/Header";
-import MapScene from "@/components/home/MapScene";
+import Header from '@/components/common/Header';
+import MapScene from '@/components/home/MapScene';
+import { Info } from '@/types/info';
+import { useEffect } from 'react';
 
+interface IProps {
+  infos: Info[];
+}
 
-export default function Home() {
+export default function Home({ infos }: IProps) {
+  useEffect(() => {
+    console.log('infos', infos);
+  }, [infos]);
   return (
     <>
       <Header />
       <MapScene />
     </>
-  )
+  );
+}
+
+// pre-rendering 해서 SSG 생성
+export async function getStaticProps() {
+  // public에 있는 info.json 가져오기
+  const infos = (await import('@/public/info.json')).default;
+  return {
+    props: { infos },
+    revalidate: 3600,
+  };
 }
